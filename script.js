20ac6dd0a94aeb169e1ef80eeb5ce2ef26c01810
Sam Grose <samugrose@gmail.com>
2020-06-20 11:24:27 -0700

    added necessary files

---
+++ script.js
@@ -0,0 +1,31 @@
+
+var cityState;
+var APIKey = "1d43caedb3b0cdb286ed6a8762ac368d";
+
+    // Here we are building the URL we need to query the database
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityState + "&appid=" + APIKey;
+
+    // We then created an AJAX call
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      // Create CODE HERE to Log the queryURL
+      console.log(queryURL);
+      // Create CODE HERE to log the resulting object
+      console.log((response.main.temp - 273.15));
+      var temp = (parseFloat(response.main.temp) - 273.15) * (9/5) + 32;
+      console.log(temp);
+      // Create CODE HERE to calculate the temperature (converted from Kelvin)
+      //var displayTemp = (temp − 273.15) * (9/5) + 32;
+      //console.log(displayTemp);
+      // Create CODE HERE to transfer content to HTML
+      var bod = $("body");
+      var newDiv = $("<div>");
+      newDiv.text("Temp in Bujumbura: " + temp.toFixed(1));
+      bod.append(newDiv);
+      // Hint: To convert from Kelvin to Fahrenheit: F = (K - 273.15) * 1.80 + 32
+      // Create CODE HERE to dump the temperature content into HTML
+
+    });
