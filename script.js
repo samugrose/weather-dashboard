39d0515240817ceff85b6d66024211f97f0d9c0d
Sam Grose <samugrose@gmail.com>
2020-06-24 09:39:43 -0700

    updated lots of things- got main card with weather info for one city loading

--- script.js
+++ script.js
@@ -1,74 +1,58 @@
 var bodGetter = $("body");
-var cityStates = ["seattle"];
+var cityStates = ["Seattle"];
 var APIKey = "1d43caedb3b0cdb286ed6a8762ac368d";
+var currentCity = cityStates[0];
 
-    // Here we are building the URL we need to query the database
     function getCityInfo() {
-        for (i = 0; i < cityStates.length; i++) {
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityStates[i] + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&cnt=5&appid=" + APIKey;
 
-    // We then created an AJAX call
+    
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
+    console.log(response);
+    $(".card-title").text(currentCity);
+    $(".mainTemp").text(((response.main.temp - 273.15) * 1.80 + 32).toFixed(1));
+    
+    console.log(response.main.humidity);
+    $(".mainHum").text(response.main.humidity);
+    $(".mainWind").text(response.wind.speed);
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    getUV(lat, lon);
     });
 }
+
+function getUV (lat, lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    $(".mainUV").text(response.value)
+  })
 }
 
-    // <div class = "page-header d-flex align-items-center justify-content-center">
-    //     <h1>
-    //        Weather Dashboard 
-    //     </h1>
-    //  </div>
-    var head = $("<div>");
-    head.attr("class","page-header d-flex align-items-center justify-content-center");
-    var headH1 = $("<h1>");
-    headH1.text("Weather Dashboard");
-    head.append(headH1); 
-    
+getCityInfo(); //when they click a new city these will be recalled
+getForecast();
 
-    //looks like an md-9 and md-3
-//     var container = $("<section>");
-//     container.addClass("container");
-//     bodGetter.prepend(container); // adds header to top, 
-//     bodGetter.prepend(head);
+function getForecast() {
+    // var queryURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + currentCity +"&cnt=5&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&cnt=5&appid=" + APIKey;
 
-//     var bigRow = $("<section>");
-//     bigRow.addClass("row");
-//     container.append(bigRow);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+        //$(".card-title").text(currentCity);
+        //$(".mainTemp").text = ((response.main.temp - 273.15) * 1.80 + 32).toFixed(1); main.temp
+      })
 
-//     var md3 = $("<section>");
-//     md3.addClass("col-md-3");
-//     bigRow.append(md3);
-//     md3.prepend($("#submitSec"));
+}
 
-    
-
-//     var md9 = $("<section>");
-//     md9.addClass("col-md-9");
-//     bigRow.append(md9);
-
-//     var md9TopRow = $("<section>");
-//     md9TopRow.addClass("row");
-//     var md9Col = $("<section>");
-//     md9Col.addClass("col-md-12");
-//     md9TopRow.append(md9Col);
-//     md9.append(md9TopRow);
-
-//     var md9BtmRow = $("<section>");
-//     md9BtmRow.addClass("row");
-//     //var md9BtmCol = $("<section>");
-//     //md9BtmCol.addClass("col-md-12"); //make this in html and append it to this md9btmrow
-//    // md9BtmRow.append($("#weatherTiles"));
-//     //md9.append(md9BtmRow);
-//     for (i = 0; i < 5; i++) {
-//         var weatherBlock = $("<section>");
-//         weatherBlock.addClass("col-md-12 d-flex justify-content-between");
-//         weatherBlock.attr("id", "weatherTiles")
-//         md9BtmRow.append(weatherBlock);
-//     }
+  
     
     jQuery(`[type = "Submit"]`).on("click", function() {
         event.preventDefault();
